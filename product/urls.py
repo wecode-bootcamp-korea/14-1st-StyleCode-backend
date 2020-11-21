7e65610520e4354d3383c68b2ce5c88911abcca2
Hyun Suk <cs982606@gmail.com>
2020-11-21 21:01:03 +0900

    Add : product/views, CategoryView 작성

--- product/urls.py
+++ product/urls.py
@@ -4,8 +4,6 @@ from .views      import CategoryView
 
 urlpatterns= [
     path('/category', CategoryView.as_view()),
-#    path('/secondcategory/<int:second_category_id>' , SecondCategoryView.as_view()),
-#    path('/thirdcategory/<int:third_category_id>'   , ThirdCategoryView.as_view())
 
 ]
 
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
 
-# Create your tests here.
+from .models import User
+
+client = Client()
+
+class UserTestCase(TestCase):
+    def test_user(self):
+        data = {
+        "login_id"   : "gustjr88" ,
+        "password"   : "dfa231s2334" ,
+        "nickname"   : "nickname22" ,
+        "email"      : "we22c2@we.com",
+        "gender_id"  : "여자" ,
+        "birth_date" : "2000-02-22"
+        }
+
+        response = client.post('/user/signup', json.dumps(data), content_type= 'application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{
+            "message:":"SUCCESS" })
+
