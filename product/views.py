d6ee95c5fe67ad5d46c38068e5189259634d55d8
ooknimm <ooknimm@gmail.com>
2020-11-23 20:52:48 +0900

    Modify: 컨벤션 수정

--- product/views.py
+++ product/views.py
@@ -28,33 +28,33 @@ class ProductDetail(View):
 
         return JsonResponse({
             'product' : {
-                'headerTopTitle'               : product.title,
-                'headerMiddleImg'      : product.main_image_url,
-                'discount'       : product.discount_rate,
-                'originPrice'       : product.price,
-                'discountPrice'      : format(int(round(product.price - (product.price * product.discount_rate),-2)),'.2f'),
-                'likeNumber'        : product.like,
-                'reviewNumber' : product.ootd.count(),
-                'mile'              : int(product.price - (product.price * product.discount_rate) * decimal.Decimal(0.05)),
-                'colors'              : [color.name for color in product.color.all()],
-                'sizes'               : [size.name for size in product.size.all()],
-                'stocks' : [{
+                'headerTopTitle'  : product.title,
+                'headerMiddleImg' : product.main_image_url,
+                'discount'        : product.discount_rate,
+                'originPrice'     : product.price,
+                'discountPrice'   : format(int(round(product.price - (product.price * product.discount_rate),-2)),'.2f'),
+                'likeNumber'      : product.like,
+                'reviewNumber'    : product.ootd.count(),
+                'mile'            : int(product.price - (product.price * product.discount_rate) * decimal.Decimal(0.05)),
+                'colors'          : [color.name for color in product.color.all()],
+                'sizes'           : [size.name for size in product.size.all()],
+                'stocks'          : [{
                     'color' : stock.color.name,
                     'size'  : stock.size.name,
                     'stock' : stock.stock_count
                 } for stock in product.stock_set.all()],
-                'headerBottomBrand'               : product.brand.name,
+                'headerBottomBrand'   : product.brand.name,
                 'brand_product_count' : product.brand.product_set.count(),
                 'headerBottomImg'     : product.brand.image_url,
                 'ootd'                : [{
-                    'author'                   : ootd.user.nickname,
-                    'authorImg' : ootd.user.profile_image_url,
-                    'description'            : ootd.description,
-                    'ootd_image_url'         : [image.image_url for image in ootd.ootdimageurl_set.all()],
-                    'like'                   : ootd.like_user.count(),
-                    'comment_count'          : ootd.comment_set.count()
+                    'author'            : ootd.user.nickname,
+                    'authorImg'         : ootd.user.profile_image_url,
+                    'description'       : ootd.description,
+                    'ootd_image_url'    : [image.image_url for image in ootd.ootdimageurl_set.all()],
+                    'like'              : ootd.like_user.count(),
+                    'comment_count'     : ootd.comment_set.count()
                 } for ootd in product.ootd.all()],
-                'articleProductDetailImgTitle'         : product.description,
+                'articleProductDetailImgTitle' : product.description,
                 'articleProductDetailImg'      : [image.image_url for image in product.productimageurl_set.all()]
             }
         }, status=200)
