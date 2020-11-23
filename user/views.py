66e2480250a30aef430776a310dbd8de0a5060d5
Hyun Suk <cs982606@gmail.com>
2020-11-23 17:43:22 +0900

    Modify : user.view수정중

--- user/views.py
+++ user/views.py
@@ -14,38 +14,39 @@ class SignUpView(View):
             login_id  = data['login_id']
             password  = data['password']
             email     = data['email']
-            profile_image_url = data.get('profile_image_url', "")
+            gender    = data['gender']
+            profile_image_url = data.get('profile_image_url', None)
 
             id_pattern = '[a-z0-9]'
-            email_pattern = '[a-za-z0-9_-]+@[a-z]+.[a-z]+'
-            password_pattern = '[a-za-z0-9]'
+            email_pattern = '[a-zA-Z0-9_-]+@[a-z]+.[a-z]+'
+            password_pattern = '[A-Za-z0-9]'
 
             if not re.search(id_pattern, login_id) or len(login_id) > 30 or len(login_id)< 3:
-                return jsonresponse ({'message':'invalid_id'}, status=400)
+                return JsonResponse ({'message':'INVALID_ID'}, status=400)
             if not re.match(email_pattern, email):
-                return jsonresponse ({'message:':'invalid_email'}, status=400)
+                return JsonResponse ({'message:':'INVALID_EMAIL'}, status=400)
             if not re.search(password_pattern, password) or len(password) > 16 or len(password) < 8:
-                return jsonresponse ({'message:':'invalid_password'}, status=400)
+                return JsonResponse ({'message:':'INVALID_PASSWORD'}, status=400)
 
-            if user.objects.filter(email=email).exists():
-                return jsonresponse ({'message:':'already_email'}, status=400)
-            if user.objects.filter(nickname=data['nickname']).exists():
-                return jsonresponse ({'message':'already_nickname'}, status=400)
+            if User.objects.filter(email=email).exists():
+                return JsonResponse ({'message:':'ALREADY_EMAIL'}, status=400)
+            if User.objects.filter(nickname=data['nickname']).exists():
+                return JsonResponse ({'message':'ALREADY_NICKNAME'}, status=400)
 
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-            user.objects.create(
+            User.objects.create(
                 login_id          = login_id,
                 password          = hashed_password,
                 nickname          = data['nickname'],
                 email             = email,
-                gender_id         = int(data['gender_id']),
+                gender_id         = 1 if data['gender']=='남자' else 2,
                 birth_date        = data['birth_date'],
                 profile_image_url = profile_image_url
             )
-            return jsonresponse ({'message:':'create_user'}, status=200)
-        except keyerror:
-            return jsonresponse({'message:':'key_error'}, status=400)
+            return JsonResponse ({'message:':'CREATE_USER'}, status=200)
+        except KeyError:
+            return JsonResponse({'message:':'KEY_ERROR'}, status=400)
 
 class LogInView(View):
     def post(self, request):
@@ -53,23 +54,23 @@ class LogInView(View):
             data     = json.loads(request.body)
             login_id = data['login_id']
             password = data['password']
-            user     = user.objects.get(login_id=login_id)
+            user     = User.objects.get(login_id=login_id)
 
             if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                token = jwt.encode({'login_id':login_id}, jwt_secret_key, algorithm=jwt_algorithm).decode('utf-8')
+                token = jwt.encode({'login_id':login_id}, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM).decode('utf-8')
 
-                return jsonresponse({'token':token}, status=200)
-            return jsonresponse({'message:':'invalid_password'}, status=400)
-        except keyerror:
-            return jsonresponse({'message:':'invalid_id'}, status=400)
-        except user.doesnotexist:
-            return jsonresponse({'message:':'user_does_not_exist'}, status=400)
+                return JsonResponse({'token':token}, status=200)
+            return JsonResponse({'message:':'INVALID_PASSWORD'}, status=400)
+        except KeyError:
+            return JsonResponse({'message:':'INVALID_ID'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'message:':'USER_DOES_NOT_EXIST'}, status=400)
 
 class ProfileView(View):
     @Login_decorator
     def get(self, request):
         try :
-            user = user.objects.get(id = request.user_id)
+            user = request.user
 
             user_info = {
                 'nickname'          : user.nickname,
@@ -80,20 +81,19 @@ class ProfileView(View):
                 'description'       : user.description,
                 'profile_image_url' : user.profile_image_url,
             }
-            return jsonresponse ({'user_info:': user_info}, status=400)
-        except keyerror:
-            return jsonresponse({'message:':'key_error'}, status=400)
+            return JsonResponse ({'user_info:': user_info}, status=200)
+        except KeyError:
+            return JsonResponse({'message:':'KEY_ERROR'}, status=400)
 
     @Login_decorator
     def put(self, request):
         try:
-            data = json.loads(request.body)
-            user = user.objects.get(id = request.user_id)
+            user = request.user
 
             if 'country' in data:
                 user.country = data['country']
             if 'gender_id' in data:
-                user.gender = data['gender_id']
+                user.gender_id = 1 if data['gender'] == "남자" else 2 ,
             if 'birth_date' in data:
                 user.birth_date = data['birth_date']
             if 'website_url' in data:
@@ -104,7 +104,8 @@ class ProfileView(View):
                 user.profile_image_url = data['profile_image_url']
             user.save()
 
-            return jsonresponse({'message:':'success'}, status=200)
-        except keyerror:
-            return jsonresponse({'message:':'key_error'}, status=400)
+            return JsonResponse({'message:':'SUCCESS'}, status=200)
+        except KeyError:
+            return JsonResponse({'message:':'KEY_ERROR'}, status=400)
+
 
