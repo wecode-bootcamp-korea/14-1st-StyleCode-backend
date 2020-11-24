b5b225f4f7b55025d80ccebe0bb455b20315810c
ooknimm <ooknimm@gmail.com>
2020-11-24 23:32:19 +0900

    Modify : 수정중

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views      import ProductDetail
+from .views      import ProductDetailView, ProductListView
 
 urlpatterns = [
-    path('/<int:product_id>', ProductDetail.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view()),
+    path('/category/<int:first_category_id>', ProductListView.as_view())
 ]
