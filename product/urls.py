ddf3bf6176feac4d00131bfd6bfc0e042474ca34
Hyun Suk <cs982606@gmail.com>
2020-11-24 15:21:32 +0900

    Add : CategoryView작성

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views      import ProductView, MdChoiceView
+from .views      import ProductView, MdChoiceView, CategoryView
 
 
 urlpatterns= [
     path('/products', ProductView.as_view()),
     path('/mdchoice', MdChoiceView.as_view()),
+    path('/categorys', CategoryView.as_view()),
 
 ]
 
