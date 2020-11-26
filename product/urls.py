88104c25bfae55581f158fcd12527b21ff13ac6a
ooknimm <ooknimm@gmail.com>
2020-11-26 16:12:22 +0900

    Modify : 카트 생성 리팩토링

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views      import ProductDetail, CategoryView, ProductView, MdChoiceView
+from .views      import ProductDetailView, CategoryView, ProductView, MdChoiceView
 
 urlpatterns = [
-    path('/<int:product_id>', ProductDetail.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view()),
     path('', ProductView.as_view()),
     path('/mdchoice/<int:mdchoice>', MdChoiceView.as_view()),
     path('/categorys', CategoryView.as_view()),
